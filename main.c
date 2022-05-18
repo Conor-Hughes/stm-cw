3dbd3cedb8c2329443b9aefbbba749964b0e9e10
(s) Conor Hughes <elch2@lunet.lboro.ac.uk>
2022-05-18 14:13:18 +0100

    Completed task!

--- main.c
+++ main.c
@@ -18,6 +18,10 @@ const int signals[8][2] = {{0,1}, {0,0}, {1,0}, {1,1}, {0,1}, {0,0}, {1,0}, {1,1
 void setup_timer_3();
 void setup_port_e();
 void setup_button_interrupt();
+void setup_pe_7_and_8_interrupt();
+void increment_counter();
+void decrement_counter();
+void set_counter();
 	
 int main(void)
 {
@@ -27,6 +31,8 @@ int main(void)
 	setup_port_e();
 	
 	setup_button_interrupt();
+	
+	setup_pe_7_and_8_interrupt();
 	
 	while (1)
   {
@@ -59,12 +65,40 @@ void setup_port_e(){
 }
 
 void setup_button_interrupt(){
-	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // Enable the interrupt on the button.
 	
+	// 2.1: Enable SysConfigController to SysClk.
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	
+	// 2.2: Remove mask to enable an interrupt to be generated from the EXTI0_IMR register
 	EXTI->IMR |= EXTI_IMR_MR0;
-	EXTI->RTSR|= EXTI_RTSR_TR0;
+	
+	// 2.3: Set interrupt trigger to be rising edge:
+	EXTI->RTSR |= EXTI_RTSR_TR0;
+	
+	// 2.4: Configure multiplexing options to enable PA.0 to generate interrupt EXTIO.
 	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
+	
+	// 2.5 Configure the NVIC to trigger the ISR.
 	NVIC_EnableIRQ(EXTI0_IRQn);
+}
+
+void setup_pe_7_and_8_interrupt()
+{
+	// Remove masks to enable interrupts from EXTI6 & EXTI7:
+	EXTI->IMR |= EXTI_IMR_MR6;
+	EXTI->IMR |= EXTI_IMR_MR7;
+	
+	// Set EXTI6 & EXTI7 ports to generate interrupt on rising and falling edges:
+	EXTI->RTSR |= EXTI_RTSR_TR6;
+	EXTI->RTSR |= EXTI_RTSR_TR7;
+	EXTI->FTSR |= EXTI_FTSR_TR6;
+	EXTI->FTSR |= EXTI_FTSR_TR7;
+	
+	// Configure multiplexing options to enable PE.6 and PE.7 to generate interrupt EXTIO.
+	SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI6_PE;
+	SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI7_PE;
+	
+	NVIC_EnableIRQ(EXTI9_5_IRQn);
 }
 
 /*
@@ -123,6 +157,112 @@ void EXTI0_IRQHandler(){
 		clockwise = !clockwise;
 	}
 }
+	
+void EXTI9_5_IRQHandler(){
+		
+	/*
+	* Channel A - PE8. Wired to PE7.
+	*	When this is rising, check to see if Channel B is low. If so, we're going clockwise.
+	*	When this is falling, check to see if Channel B is high. If so, we're going clockwise.
+	*/
+	if (EXTI->PR & EXTI_PR_PR7) // check source
+	{
+		EXTI->PR |= EXTI_PR_PR7; // clear flag
+		
+		// If this was a rising edge:
+		if(GPIOE->IDR & (1<<7)){
+						
+			// Check if Channel B is low. If it is, we can increment by 1. Else, decrement 1.
+			if(GPIOE->IDR & (1<<6)){
+				decrement_counter();
+			}
+			else {
+				increment_counter();
+			}
+		}
+		// Else, this is a falling edge:
+		else {
+			
+			// Check if Channel B is high. If it is, we can increment by 1. Else, decrement 1.
+			if(GPIOE->IDR & (1<<6)){
+				increment_counter();
+			}
+			else {
+				decrement_counter();
+			}
+			
+		}
+	}
+	
+	/*
+	* Channel B - PE9. Wired to PE6.
+	*	When this is rising, check to see if Channel B is high. If so, we're going clockwise.
+	*	When this is falling, check to see if Channel B is low. If so, we're going clockwise.
+	
+	if (EXTI->PR & EXTI_PR_PR6) // check source
+	{
+		EXTI->PR |= EXTI_PR_PR6; // clear flag*
+		
+		if(GPIOE->IDR & (1<<6)){
+			// If this was a rising edge, turn on PE11:
+			GPIOE->BSRRL = 0x800;
+		}
+		else {
+			// If this was a falling edge, turn off PE11:
+			GPIOE->BSRRH = 0x800;
+		}
+	}*/
+}
+
+int encoderCount = 0;
+
+void adjust__counter(bool add){
+	
+	if(add){
+		encoderCount++;
+	}
+	else {
+		encoderCount--;
+	}
+	
+	
+	if(encoderCount > 15){
+		encoderCount = 0;
+	}
+	
+	// Reset all LEDS from PE11 -> PE14.
+	GPIOE->BSRRH &= ~(0x7800);
+	
+	GPIOE->BSRRL |= encoderCount << 11;
+}
+
+void increment_counter(){
+	encoderCount++;
+	
+	if(encoderCount > 15){
+		encoderCount = 15;
+	}
+	
+	set_counter();
+}
+
+void decrement_counter(){
+	encoderCount--;
+	
+	if(encoderCount < 0){
+		encoderCount = 0;
+	}
+	
+	set_counter();
+}
+
+void set_counter(){
+	// Reset all LEDS from PE11 -> PE14.
+	GPIOE->BSRRH |= 0x7800;
+	
+	GPIOE->BSRRL |= encoderCount << 11;
+}
+
 
 
 
